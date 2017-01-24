ae487f6ce61daa58611e9494c65da0eb301fbfda
Jesse Farmer <jessethebuilder@yahoo.com>
2017-01-24 02:47:34 -0800

    publish

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,14 @@
 class ListingsController < ApplicationController
   def index
-    if params[:only_new]
-      @listings = Listing.where(:seen => false)
-    elsif params[:unvisited]
-      @listings = Listing.where(:visited => false)
-    else
+    if params[:search]
+      search = params[:search].split(',')
+      @listings = Listing.where(search.first => search.last)
+    elsif params[:all]
       @listings = Listing.all
+    elsif params[:minutes_old]
+      @listings= Listing.where(created_at: (params[:minutes_old].to_i.minutes.ago..Time.now))
+    else
+      @listings = Listing.where(:seen => false)
     end
 
     @listings.each{ |l| l.update(seen: true) }
