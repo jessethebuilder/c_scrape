b77bbb964c9527b178da0f97caf8a4d042c554f6
Jesse Farmer <jessethebuilder@yahoo.com>
2017-02-22 14:12:44 -0800

    pay

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,34 +1,22 @@
 class ListingsController < ApplicationController
   def index
-    # if params[:search]
-    #   search = params[:search].split(',')
-    #   @listings = Listing.where(search.first => search.last)
-    # elsif params[:all]
-    #   @listings = Listing.all
-    # elsif params[:minutes_old]
-    #   @listings= Listing.where(created_at: (params[:minutes_old].to_i.minutes.ago..Time.now))
-    # else
-    #   @listings = Listing.where(:seen => false)
+    # listings = Listing.all.order(created_at: :desc).page(params[:page]).per(50)
+    # @listings = []
+    # urls = []
+    #
+    # listings.each do |l|
+    #   url = l.url
+    #   unless urls.include?(url)
+    #     @listings << l
+    #   end
+    #
+    #   urls << url
     # end
     #
-    # @listings = @listings.order(:created_at => :desc)
-    #
-    # @listings.each{ |l| l.update(seen: true) }
+    # @listings = Kaminari.paginate_array(@listings).page(params[:page]).per(100)
 
-    listings = Listing.all.order(created_at: :desc).page(params[:page]).per(50)
-    @listings = []
-    urls = []
+    @listings = Listing.all.order(created_at: :desc).page(params[:page]).per(50)
 
-    listings.each do |l|
-      url = l.url
-      unless urls.include?(url)
-        @listings << l
-      end
-
-      urls << url
-    end
-
-    @listings = Kaminari.paginate_array(@listings).page(params[:page]).per(100)
   end
 
   def show
